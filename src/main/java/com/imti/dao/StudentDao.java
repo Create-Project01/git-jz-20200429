d7f3030a718545de6a2cd22c3fe37e75ef1531d9
hp <hp@LAPTOP-JH1ORQR3>
2020-04-30 17:01:46 +0800

    修改文件Student.java 添加StudentDao.java

--- src/main/java/com/imti/dao/StudentDao.java
+++ src/main/java/com/imti/dao/StudentDao.java
@@ -6,4 +6,8 @@ public class StudentDao {
     public void insertStudent(Student student) {
     	System.out.println("insert Student"+ student);
     }
+    
+    public void deleteStudent(Integer id) {
+    	System.out.println("delete Student"+id);
+    }
 }
