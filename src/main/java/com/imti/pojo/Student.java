2659dd90414f7cfa5f9b9fb70bc3e381c238836d
hp <hp@LAPTOP-JH1ORQR3>
2020-04-30 16:54:18 +0800

    修改Student.java 添加StudentDao.java

--- src/main/java/com/imti/pojo/Student.java
+++ src/main/java/com/imti/pojo/Student.java
@@ -4,6 +4,16 @@ public class Student {
     private String name;        
     //edit by Create-Project02
     //edit by Create-Project01
+    
+    private Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
